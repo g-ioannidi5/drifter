886102ec1b4a363a82ce39cf0d575f085042184c
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-11-09 19:58:27 +0000

    Setting up main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  root 'login#index'
   get 'home', to: 'application#home'
   get 'editProfile', to: 'application#editProfile'
   get 'friends', to: 'application#friends'
   get 'messages', to: 'application#messages'
+
 end
