8bdab61f2ab7a12dba4804392112512fab211ffa
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-11-09 12:55:52 +0000

    Declared some more javascripts header fully responsive working :smile:

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( default.min.css )
 
+Rails.application.config.assets.precompile += %w( vendor.min.js )
+
+Rails.application.config.assets.precompile += %w( scripts.min.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
