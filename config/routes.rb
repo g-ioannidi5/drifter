b6e163d9b2be008cafdb31493274a3db281bbee5
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-11-09 19:40:49 +0000

    Added messages section

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'home', to: 'application#home'
   get 'editProfile', to: 'application#editProfile'
   get 'friends', to: 'application#friends'
+  get 'messages', to: 'application#messages'
 end
