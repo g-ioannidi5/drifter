63becd38ce51f118e9268d09c10f13192a9a17a3
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-12-09 22:42:47 +0000

    Working to solve issue #8

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,6 +17,8 @@ Rails.application.config.assets.precompile += %w( scripts.js )
 
 Rails.application.config.assets.precompile += %w( iconFile.js )
 
+Rails.application.config.assets.precompile += %w( endless.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
